d18e3eff38295265239fec92eea6964231bb501f
OFFICIALNITIN <official.nitin105@gmail.com>
2024-05-22 19:48:11 +0530

    changed server endpoint proxy

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -1,25 +1,41 @@
-import React, { useContext, useState } from 'react'
-import './Login.css'
-import { AuthContext } from '../../context/authContext/AuthContext'
-import { login } from '../../context/authContext/apiCalls'
+import React, { useContext, useState } from "react";
+import "./Login.css";
+import { AuthContext } from "../../context/authContext/AuthContext";
+import { login } from "../../context/authContext/apiCalls";
 
 export const Login = () => {
-    const [ email, setEmail] = useState('')
-    const [ password, setPassword] = useState('')
-    const {isFetching, dispatch} = useContext(AuthContext)
-    
-    const handleLogin = (e) => {
-        e.preventDefault()
-        login({email,password}, dispatch)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { isFetching, dispatch } = useContext(AuthContext);
 
-    }
+  const handleLogin = (e) => {
+    e.preventDefault();
+    login({ email, password }, dispatch);
+  };
   return (
-    <div className='login'>
-        <div className='loginForm'>
-            <input type='text' placeholder='email' className='loginInput' onChange={(e)=> setEmail(e.target.value)}/>
-            <input type='password' placeholder='password' className='loginInput' onChange={(e)=> setPassword(e.target.value)}/>
-            <button className='loginButton' onClick={handleLogin} disabled={isFetching}>Login</button>
-        </div>
+    <div className="login">
+      <div className="loginForm">
+        <p className="admin_login_title">Admin Login</p>
+        <input
+          type="text"
+          placeholder="email"
+          className="loginInput"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="loginInput"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          className="loginButton"
+          onClick={handleLogin}
+          disabled={isFetching}
+        >
+          Login
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
