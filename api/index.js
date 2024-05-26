0699e9a0c38861b584306f493221c077f0f152f1
OFFICIALNITIN <official.nitin105@gmail.com>
2024-05-26 10:59:19 +0530

    made some changes

--- api/index.js
+++ api/index.js
@@ -22,9 +22,14 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization",
+    "token"
+  );
   res.setHeader("Access-Control-Allow-Credentials", "true");
   if (req.method === "OPTIONS") {
+    res.setHeader("Access-Control-Max-Age", "86400");
     res.sendStatus(204); // No Content
   } else {
     next();
